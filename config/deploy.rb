f37c54d7e732e25929ad2da413990fbb57a324a3
yukino0307 <62429496+yukino0307@users.noreply.github.com>
2020-04-08 23:35:49 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.13.0'
 
 # Capistranoのログの表示に利用する
-set :application, 'chat-space''
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:yukino0307/chat-space.git'
@@ -31,4 +31,5 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
 end
